c4de18b2c5c7fcd29ccc83bde9be660dc6ea0ad9
Westpark Communications <westparkcom@freeswitch-dev.westpark.local>
2016-12-28 14:24:45 -0600

    Fix datetime ref

--- utilities/archiveacct.py
+++ utilities/archiveacct.py
@@ -56,7 +56,7 @@ except:
     
 if capDateExist:
     try:
-        capDate = str(datetime.strptime(capDate, "%Y/%m/%d"))
+        str(datetime.strptime(capDate, "%Y/%m/%d"))
     except:
         print ""
         print "Incorrect date format for --capdate, please use YYYY/MM/DD"
